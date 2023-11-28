836e349240c8527991e2e23eec38fbc06b77966d
Francisco Quintanilla <francisco.quintanilla@verndale.com>
2023-11-28 10:34:16 -0500

    Remove first div

--- app/components/HeroBlock.tsx
+++ app/components/HeroBlock.tsx
@@ -4,7 +4,13 @@ import React from "react";
 const HeroBlock = (props: any) => {
   var item = props.props;
 
-  return <h1 className="text-5xl font-bold">{item?.Title}</h1>;
+  return (
+    <div className="hero-content text-center">
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">{item?.Title}</h1>
+      </div>
+    </div>
+  );
 };
 
 function setRaw(html: string) {
