29b96417aec8eb558949e3ddeff91973ab6db288
Francisco Quintanilla <francisco.quintanilla@verndale.com>
2023-11-28 10:28:15 -0500

    Remove containers

--- app/components/HeroBlock.tsx
+++ app/components/HeroBlock.tsx
@@ -4,15 +4,7 @@ import React from "react";
 const HeroBlock = (props: any) => {
   var item = props.props;
 
-  return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">{item?.Title}</h1>
-        </div>
-      </div>
-    </div>
-  );
+  return <h1 className="text-5xl font-bold">{item?.Title}</h1>;
 };
 
 function setRaw(html: string) {
